2ac50c8ae2a5f21954d8a0316ff75ee7b5e2793f
Ryu Alv <ryualvino57@gmail.com>
2024-12-10 13:44:18 +0700

    fix: change code response for login from 201 to 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Request, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
@@ -9,6 +16,7 @@ export class AuthController {
   //route
   //POST /auth/login
   @Post('login')
+  @HttpCode(200)
   @UseGuards(LocalAuthGuard)
   async login(
     @Request() req: any,
