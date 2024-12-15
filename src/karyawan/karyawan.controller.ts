3abe3406a7a00d518afd2204eaf9c0b4c9968590
Ryu Alv <ryualvino57@gmail.com>
2024-12-15 11:09:37 +0700

    feat: add authentication and authorization

--- src/karyawan/karyawan.controller.ts
+++ src/karyawan/karyawan.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Query,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { KaryawanService } from './karyawan.service';
 import {
@@ -15,26 +16,34 @@ import {
   FindAllKaryawanDto,
   UpdateKaryawanDto,
 } from './dto/create-karyawan.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { Roles } from 'src/auth/custom_decorator/role.decorator';
 
+@UseGuards(JwtAuthGuard, RoleGuard)
 @Controller('api/karyawan')
 export class KaryawanController {
   constructor(private readonly karyawanService: KaryawanService) {}
 
+  @Roles('superadmin', 'adminworkshop')
   @Post()
   create(@Body() createKaryawanDto: CreateKaryawanDto) {
     return this.karyawanService.handleCreateKaryawan(createKaryawanDto);
   }
 
+  @Roles('superadmin', 'adminworkshop')
   @Get()
   findAll(@Query() query: FindAllKaryawanDto) {
     return this.karyawanService.handleFindAllKaryawan(query);
   }
 
+  @Roles('superadmin', 'adminworkshop')
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.karyawanService.handleFindOneKaryawan(id);
   }
 
+  @Roles('superadmin', 'adminworkshop')
   @Put(':id')
   update(
     @Param('id') id: number,
@@ -43,6 +52,7 @@ export class KaryawanController {
     return this.karyawanService.handleUpdateKaryawan(id, updateKaryawanDto);
   }
 
+  @Roles('superadmin', 'adminworkshop')
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.karyawanService.hanldeDeleteKaryawan(id);
